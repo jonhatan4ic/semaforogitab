19d26ea4179eff3cd33dcca9ede175b7a5bcd0b1
Diurno <Diurno@C8PC3.informatica.comercio>
2021-02-18 12:40:23 +0100

    Inicial

---
+++ MacroproyectoZJ/src/main/java/Habitats.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Diurno
+ */
+public class Habitats {
+        private String nombre;
+        private int dato;
+        private double poblacion;
+    public Habitats()
+    {
+        nombre = "juan";
+        dato = 2;
+        poblacion = 24.6;
+    }
+    public void getDatos()
+    {
+        System.out.println(dato);
+    }
+}
