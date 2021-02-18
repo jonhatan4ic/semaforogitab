97b1dcd75dbb071ab0e0ac0e563fc70dc9fe0679
Diurno <Diurno@C8PC3.informatica.comercio>
2021-02-18 11:47:21 +0100

    nuevo doucmetno

---
+++ copiaSeguridad2/src/main/java/com/mycompany/copiaseguridad2/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.copiaseguridad2;
+
+/**
+ *
+ * @author Diurno
+ */
+public class NewClass {
+    public NewClass()
+    {
+        String embudo;
+    }
+}
